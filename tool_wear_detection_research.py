090e5c1f635888be4b0fecce75c788d79289f29a
MAAykanat <m.a.aykanat@gmail.com>
2024-05-03 10:19:12 +0300

    Set-up pd frame

---
+++ tool_wear_detection_research.py
@@ -0,0 +1,10 @@
+import numpy as no
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_columns', None) # Show all the columns
+pd.set_option('display.max_rows', None) # Show all the rows
+pd.set_option('max_colwidth', None) # Show all the text in the columns
+pd.set_option('display.float_format', lambda x: '%.5f' % x) # Show all the decimals
+
