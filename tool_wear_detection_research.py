a84349a0c82fd13da9d120759b74a8efd8792a9a
MAAykanat <m.a.aykanat@gmail.com>
2024-05-03 17:07:30 +0300

    Load all dataset including train-set

--- tool_wear_detection_research.py
+++ tool_wear_detection_research.py
@@ -8,3 +8,20 @@ pd.set_option('display.max_rows', None) # Show all the rows
 pd.set_option('max_colwidth', None) # Show all the text in the columns
 pd.set_option('display.float_format', lambda x: '%.5f' % x) # Show all the decimals
 
+# Load the dataset
+
+df_train = pd.read_csv('dataset/train.csv')
+
+total = 0
+
+for i in range(1,10):
+    globals()['df_%s' % i] = pd.read_csv(f'dataset/experiment_0{i}.csv')
+    total =total + globals()['df_%s' % i].shape[0]
+
+for i in range(10,19):
+    globals()['df_%s' % i] = pd.read_csv(f'dataset/experiment_{i}.csv')
+    total =total + globals()['df_%s' % i].shape[0]
+
+for i in range(1,19):
+    print(f"df_{i} shape: {globals()['df_%s' % i].shape}")
+
