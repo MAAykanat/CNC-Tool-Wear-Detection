1e1f0a16bb91585fc8ae6234b19e5b02797b4302
MAAykanat <m.a.aykanat@gmail.com>
2024-05-03 17:08:48 +0300

    General picture of trainset and missing_value handle

--- tool_wear_detection_research.py
+++ tool_wear_detection_research.py
@@ -25,3 +25,55 @@ for i in range(10,19):
 for i in range(1,19):
     print(f"df_{i} shape: {globals()['df_%s' % i].shape}")
 
+
+#######################################
+### EXPLORATORY DATA ANALYSIS - EDA ###
+#######################################
+
+# 1. General Picture of the Dataset
+# 2. Catch Numeric and Categorical Value
+# 3. Catetorical Variables Analysis
+# 4. Numeric Variables Analysis
+# 5. Target Variable Analysis (Dependent Variable) - Categorical
+# 6. Target Variable Analysis (Dependent Variable) - Numeric
+# 7. Outlier Detection
+# 8. Missing Value Analysis
+# 9. Correlation Matrix
+
+#############
+# Train SET #
+#############
+
+# 1. General Picture of the Dataset
+# 2. Missing Value Analysis - Handle
+
+# 1. General Picture of the Dataset
+def check_df(dataframe, head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(head))
+    print("##################### Tail #####################")
+    print(dataframe.tail(head))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+check_df(df_train)
+
+# 2. Missing Value Analysis - Handle
+
+print(df_train.isnull().sum())
+
+# There are 4 missing values in passed_visual_inspection column
+
+# Fill NaN values with "no"
+
+df_train["passed_visual_inspection"] = df_train["passed_visual_inspection"].fillna("no")
+
+print(df_train.isnull().sum())
+
+
