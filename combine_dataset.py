43e73e574e4b63d3fcd4eca2ef3ace3add940ca6
MAAykanat <m.a.aykanat@gmail.com>
2024-05-06 09:57:09 +0300

    Train missing value handle

--- combine_dataset.py
+++ combine_dataset.py
@@ -17,4 +17,15 @@ for i in range(10,19):
 
 for i in range(1,19):
     print(f"df_{i} shape: {globals()['df_%s' % i].shape}")
+    # print(f"df_{i} columns: {globals()['df_%s' % i].isnull().sum()}") # No missing values
+
+# Missing Value Handle
+print(df_train.isnull().sum()) # There are 4 missing values in passed_visual_inspection column
+
+# Fill missing values with "no"
+
+df_train['passed_visual_inspection'] = df_train['passed_visual_inspection'].fillna('no')
+
+# Check the missing values again
+print(df_train.isnull().sum()) # There are no missing values in the dataset
 
