f9dc5830a85a46e769619ef155140696142c2e3f
MAAykanat <m.a.aykanat@gmail.com>
2024-05-06 11:41:25 +0300

    EDA-1. General Picture of dataset

--- tool_wear_detection_research.py
+++ tool_wear_detection_research.py
@@ -1,12 +1,18 @@
-import numpy as no
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+from shutil import get_terminal_size
+import warnings
+
+warnings.filterwarnings('ignore')
+
 pd.set_option('display.max_columns', None) # Show all the columns
 pd.set_option('display.max_rows', None) # Show all the rows
 pd.set_option('max_colwidth', None) # Show all the text in the columns
-pd.set_option('display.float_format', lambda x: '%.5f' % x) # Show all the decimals
+pd.set_option('display.float_format', lambda x: '%.3f' % x) # Show all the decimals
+pd.set_option('display.width', get_terminal_size()[0]) # Get bigger terminal display width
 
 # Load the dataset
 df = pd.read_csv('dataset/combined.csv')
@@ -27,4 +33,33 @@ df.columns = df.columns.str.upper()
 # 7. Outlier Detection
 # 8. Missing Value Analysis
 # 9. Correlation Matrix
+
+# 1. General Picture of the Dataset
+
+def check_df(dataframe, head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(head))
+    print("##################### Tail #####################")
+    print(dataframe.tail(head))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+check_df(df)
+
+# Shape: (25286, 52)
+"""
+MACHINING_PROCESS             object
+EXP_NO                         int64
+FEEDRATE                       int64
+CLAMP_PRESSURE               float64
+TARGET                        object
+Others are                   float64
+"""
+# No missing values in the dataset
 
