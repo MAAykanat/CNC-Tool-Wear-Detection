7dfd59ac7ab4877e7e5f39dbab15f1be51fb52f3
MAAykanat <m.a.aykanat@gmail.com>
2024-05-06 10:00:45 +0300

    Combine datasets

--- combine_dataset.py
+++ combine_dataset.py
@@ -19,7 +19,10 @@ for i in range(1,19):
     print(f"df_{i} shape: {globals()['df_%s' % i].shape}")
     # print(f"df_{i} columns: {globals()['df_%s' % i].isnull().sum()}") # No missing values
 
-# Missing Value Handle
+########################
+# Missing Value Handle #
+########################
+
 print(df_train.isnull().sum()) # There are 4 missing values in passed_visual_inspection column
 
 # Fill missing values with "no"
@@ -29,3 +32,21 @@ df_train['passed_visual_inspection'] = df_train['passed_visual_inspection'].fill
 # Check the missing values again
 print(df_train.isnull().sum()) # There are no missing values in the dataset
 
+##########################
+# Combine all dataframes #
+##########################
+
+frames= []
+for i in range(1,19):
+    globals()['df_%s' % i]["target"]="worn" if df_train["tool_condition"][i-1] == "worn" else "unworn"
+    frames.append(globals()['df_%s' % i])
+
+df = pd.concat(frames, axis=0, ignore_index=True)
+
+print(df.head())
+print(df.shape)
+
+
+# Save the dataset
+
+# df_train.to_csv('dataset/combined.csv', index=False)
