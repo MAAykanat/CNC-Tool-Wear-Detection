f9a753c6f1dd1fc72e10cc092e9ea1c20d0fbd7a
MAAykanat <m.a.aykanat@gmail.com>
2024-05-07 11:46:19 +0300

    Hyperparameter optimazition

--- tool_wear_model.py
+++ tool_wear_model.py
@@ -1,11 +1,13 @@
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 import warnings
 from shutil import get_terminal_size
 
 from sklearn.model_selection import train_test_split, cross_validate
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
@@ -81,4 +83,84 @@ def base_models(X, y, scoring="roc_auc", cv=10, all_metrics=False):
             f.writelines(f"Score: {round(cv_results['test_score'].mean(), 4)} ({name})\n")
             f.close()
 
-base_models(X, y, scoring=["accuracy", "f1", "roc_auc" ], all_metrics=True)
+# base_models(X, y, scoring=["accuracy", "f1", "roc_auc" ], all_metrics=True)
+
+# 2. Automated Hyperparameter Optimization
+knn_params = {"n_neighbors": range(2, 50)}
+
+cart_params = {'max_depth': range(1, 20),
+               "min_samples_split": range(2, 30)}
+
+rf_params = {"max_depth": [8, 15, None],
+             "max_features": [5, 7, "auto"],
+             "min_samples_split": [15, 20],
+             "n_estimators": [200, 300]}
+
+xgboost_params = {"learning_rate": [0.1, 0.01],
+                  "max_depth": [5, 8],
+                  "n_estimators": [100, 200]}
+
+lightgbm_params = {"learning_rate": [0.01, 0.1],
+                   "n_estimators": [300, 500]}
+
+
+classifiers = [('KNN', KNeighborsClassifier(), knn_params),
+               ("CART", DecisionTreeClassifier(), cart_params),
+               ("RF", RandomForestClassifier(), rf_params),
+                ('XGBoost', XGBClassifier(use_label_encoder=False, eval_metric='logloss'), xgboost_params),
+               ('LightGBM', LGBMClassifier(), lightgbm_params)]
+
+
+def hyperparameter_optimization(X, y, classifiers, cv=3, scoring="roc_auc", all_metrics=False):
+    print("Hyperparameter Optimization....")
+    best_models = {}
+
+    if (all_metrics == True):
+        for name, classifier, params in classifiers:
+            print(f"########## {name} ##########")
+            cv_results = cross_validate(classifier, X, y, cv=cv, scoring=scoring, n_jobs=-1)
+
+            print(f"Accuracy (Before): {round(cv_results['test_accuracy'].mean(), 4)}")
+            print(f"F1 (Before): {round(cv_results['test_f1'].mean(), 4)}")
+            print(f"ROC_AUC (Before): {round(cv_results['test_roc_auc'].mean(), 4)}")
+
+            gs_best = GridSearchCV(classifier, params, cv=cv, n_jobs=-1, verbose=False).fit(X, y)
+            final_model = classifier.set_params(**gs_best.best_params_)
+
+            cv_results = cross_validate(final_model, X, y, cv=cv, scoring=scoring, n_jobs=-1)
+
+            print(f"Accuracy: {round(cv_results['test_accuracy'].mean(), 4)} ({name}) ")
+            print(f"F1: {round(cv_results['test_f1'].mean(), 4)} ({name}) ")
+            print(f"ROC_AUC: {round(cv_results['test_roc_auc'].mean(), 4)} ({name})")
+            print(f"{name} best params: {gs_best.best_params_}", end="\n\n")
+
+            f = open('Estimators_Hyperparameter.txt', 'a')
+            f.writelines(f"Accuracy: {round(cv_results['test_accuracy'].mean(), 4)} ({name})\n")
+            f.writelines(f"F1: {round(cv_results['test_f1'].mean(), 4)} ({name})\n")
+            f.writelines(f"ROC_AUC: {round(cv_results['test_roc_auc'].mean(), 4)} ({name})\n")
+            f.writelines(f"{name} best params: {gs_best.best_params_}\n")
+            f.close()
+
+            best_models[name] = final_model
+    else:
+        for name, classifier, params in classifiers:
+            print(f"########## {name} ##########")
+            cv_results = cross_validate(classifier, X, y, cv=cv, scoring=scoring)
+            print(f"{scoring} (Before): {round(cv_results['test_score'].mean(), 4)}")
+
+            gs_best = GridSearchCV(classifier, params, cv=cv, n_jobs=-1, verbose=False).fit(X, y)
+            final_model = classifier.set_params(**gs_best.best_params_)
+
+            cv_results = cross_validate(final_model, X, y, cv=cv, scoring=scoring)
+
+            print(f"{scoring} (After): {round(cv_results['test_score'].mean(), 4)}")
+            print(f"{name} best params: {gs_best.best_params_}", end="\n\n")
+
+            f = open('Estimators_Hyperparameter.txt', 'a')
+            f.writelines(f"{scoring} (After): {round(cv_results['test_score'].mean(), 4)}\n")
+            f.writelines(f"{name} best params: {gs_best.best_params_}\n")
+            f.close()
+            best_models[name] = final_model
+    return best_models
+
+best_models = hyperparameter_optimization(X, y, classifiers=classifiers, cv=10, scoring=["accuracy", "f1", "roc_auc" ], all_metrics=True)
